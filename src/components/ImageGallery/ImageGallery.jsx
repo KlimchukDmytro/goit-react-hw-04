5e181c55b2c44555514760655179e1397627e71f
Dmytro Klimchuk <d664433334@gmail.com>
2024-11-13 22:30:36 +0200

    start

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,18 @@
+import ImageCard from "../ImageCard/ImageCard";
+
+const ImageGallery = () => {
+  return (
+    <div>
+      <ul>
+        {/* Набір елементів списку із зображеннями */}
+        <li>
+          <div>
+            <img src="" alt="" />
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ImageGallery;
